f8f8cf5d3f244cc515084ee1ac35ed05df93fb89
Maher Majdoub <maher.majjdoub@gmail.com>
2026-05-28 01:11:46 +0100

    doc: document sstable file format

--- include/lsm/sstable/base_sstable.h
+++ include/lsm/sstable/base_sstable.h
@@ -1,3 +1,34 @@
+/**
+  SSTable File Format
+  ==========================
+
+  [ Data Block 0 ] [ Data Block 1 ] .... [ Indexes Block ] [ Footer ]
+
+  Data Block 
+  ---------------
+  [ Record Header | Key(string) | Value(string) ]
+
+  RecordHeader 
+    - size_t key_size
+    - size_t value_size
+
+  
+  Indexes Block 
+  ---------------
+  [ Data Block 0 index ] [Data Block 1 index] .....
+
+  Data Block Index
+    - offset_t block_start
+    - offset_t block_end
+
+  
+  Footer
+  ---------------
+    - offset_t indexes_bloc_start_offset
+    - size_t indexes_bloc_size
+*/
+
+
 #pragma once 
 
 #include <cstddef>
